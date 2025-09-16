bd17b65b85ee91c623f5b0a89cd8d53b3573ba19
gbuchida <gabrieluchidacontato@gmail.com>
2025-09-16 10:36:25 -0300

    Update atv31.php

--- atv31.php
+++ atv31.php
@@ -12,6 +12,17 @@ Componentes:
  25324764-2 - Gabriel Eduardo Diniz
  25016187-2 - Michel Bueno Chacon
  25001459-2 - Eduardo Gritten dos Santos Spohr
-Data:
-Descritivo:
+Data: 16/09/2025
+Descritivo: Crie uma condição que verifique se uma pessoa é maior de idade
 *******************************************************************************/
+
+<?php
+echo "Digite sua idade:  ";
+$idade = (int) trim(fgets(STDIN));
+
+if ($idade >= 18) {
+    echo "Você é maior de idade.\n";
+} else {
+    echo "Você é menor de idade.\n";
+}
+?>
